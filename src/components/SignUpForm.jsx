ad075c261546243050c24a3f7a390fce71777066
Salar Gholami <salargholami021@gmail.com>
2023-06-22 05:24:11 +0330

    validate inputs form and show errors to user

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -10,7 +10,9 @@ const onSubmit = (values) => console.log(values);
 const validate = (values) => {
   let errors = {};
 
-  if (!values.name) errors.name = "Name is requierd";
+  if (!values.name) {
+    errors.name = "Name is requierd";
+  }
 
   if (!values.email) {
     errors.email = "Email is requierd";
@@ -29,9 +31,7 @@ const SignUpForm = () => {
     onSubmit,
     validate,
   });
-
-  console.log(formik.errors);
-
+ 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
@@ -40,27 +40,39 @@ const SignUpForm = () => {
           <input
             type="text"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.name}
             name="name"
           />
+          {formik.errors.name && formik.touched.name && (
+            <div className="error">{formik.errors.name}</div>
+          )}
         </div>
         <div className="formControl">
           <label>Email</label>
           <input
             type="text"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.email}
             name="email"
           />
+          {formik.errors.email && formik.touched.email && (
+            <div className="error">{formik.errors.email}</div>
+          )}
         </div>
         <div className="formControl">
           <label>Password</label>
           <input
             type="text"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.password}
             name="password"
           />
+          {formik.errors.password && formik.touched.password && (
+            <div className="error">{formik.errors.password}</div>
+          )}
         </div>
         <button type="submit">submit</button>
       </form>
