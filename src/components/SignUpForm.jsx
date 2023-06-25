ebbf10062c3bd6cfaf554ea5a569a457b872420c
Salar Gholami <salargholami021@gmail.com>
2023-06-25 03:56:59 +0330

    add some fileds and validate inputs

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,8 +1,14 @@
 import { useFormik } from "formik";
-import { object, string } from "yup";
+import { object, ref, string } from "yup";
 
 // 1.mange state
-const initialValues = { name: "", email: "", password: "" };
+const initialValues = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  passwordConfirm: "",
+};
 
 // 2.handler submiission
 const onSubmit = (values) => console.log(values);
@@ -11,7 +17,19 @@ const onSubmit = (values) => console.log(values);
 const validationSchema = object({
   name: string().required("Name is requierd"),
   email: string().email("invalid Email format").required("Email is requierd"),
-  password: string().required("Password is requierd"),
+  phoneNumber: string()
+    .required("phone number is requierd")
+    .min(11, "number must have at least 11 characters"),
+  password: string()
+    .required("Password is requierd")
+    .min(8, "Password must have at least 8 characters")
+    .matches(/[0-9]/, "digit")
+    .matches(/[a-z]/, "lowercase")
+    .matches(/[A-Z]/, "uppercase")
+    .matches(/[@]/, "@!@#$%^&*"),
+  passwordConfirm: string()
+    .required("Password is requierd")
+    .oneOf([ref("password")], "Passwords does not match"),
 });
 
 const SignUpForm = () => {
@@ -39,14 +57,36 @@ const SignUpForm = () => {
           )}
         </div>
         <div className="formControl">
+          <label>phone Number</label>
+          <input
+            type="number"
+            {...formik.getFieldProps("phoneNumber")}
+            name="phoneNumber"
+          />
+          {formik.errors.phoneNumber && formik.touched.phoneNumber && (
+            <div className="error">{formik.errors.phoneNumber}</div>
+          )}
+        </div>
+        <div className="formControl">
           <label>Password</label>
           <input
-            type="password"
+            type="text"
             {...formik.getFieldProps("password")}
             name="password"
           />
           {formik.errors.password && formik.touched.password && (
             <div className="error">{formik.errors.password}</div>
+          )}
+        </div>
+        <div className="formControl">
+          <label>Password Confirm</label>
+          <input
+            type="text"
+            {...formik.getFieldProps("passwordConfirm")}
+            name="passwordConfirm"
+          />
+          {formik.errors.passwordConfirm && formik.touched.passwordConfirm && (
+            <div className="error">{formik.errors.passwordConfirm}</div>
           )}
         </div>
         <button type="submit">submit</button>
